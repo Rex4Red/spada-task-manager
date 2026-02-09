286a2140267f17fd2346718552535206e9a4c545
ptr <putrasananda61@gmail.com>
2026-02-09 20:57:04 +0700

    fix: add executablePath to scraperService for HF Chrome

--- backend/src/services/scraperService.ts
+++ backend/src/services/scraperService.ts
@@ -13,6 +13,7 @@ export class ScraperService {
             console.log('Launching Puppeteer...');
             this.browser = await puppeteer.launch({
                 headless: true,
+                executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
                 args: [
                     '--no-sandbox',
                     '--disable-setuid-sandbox',
